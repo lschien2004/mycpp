730c5171c5316785cb3b7311932ca16686cd1d54
lschien2004 <lschien2004@gmail.com>
2019-09-23 17:08:46 +0800

    Add files via upload

--- mystr.h
+++ mystr.h
@@ -11,99 +11,110 @@ typedef struct {
 	int p2;
 } PO2;
 
-static PO2 sp2;
-static char Emptystr[]="";
+int WdIdxInStr(const char *str,const char *word);
+int WdIdxInStr(const int si,const char *str,const char *word);
+int WdIdxInStr(const int si,const int ei,const char *str,const char *word);
+int WdIdxInStrRev(const char *str,const char *word);
+int WdIdxInStrRev(const int si,const char *str,const char *word);
+int WdIdxInStrRev(const int si,const int ei,const char *str,const char *word);
 
-int WordIdxInStr(const char *str,const char *word);
-int WordIdxInStr(const int si,const char *str,const char *word);
-int WordIdxInStr(const int si,const int ei,const char *str,const char *word);
-int WordIdxInStrRev(const char *str,const char *word);
-int WordIdxInStrRev(const int si,const char *str,const char *word);
-int WordIdxInStrRev(const int si,const int ei,const char *str,const char *word);
+int WdIdxInStr(const char *str,const char c);
+int WdIdxInStr(const int si,const char *str,const char c);
+int WdIdxInStr(const int si,const int ei,const char *str,const char c);
+int WdIdxInStrRev(const char *str,const char c);
+int WdIdxInStrRev(const int si,const char *str,const char c);
+int WdIdxInStrRev(const int si,const int ei,const char *str,const char c);
 
-int WordIdxInStr(const char *str,const char c);
-int WordIdxInStr(const int si,const char *str,const char c);
-int WordIdxInStr(const int si,const int ei,const char *str,const char c);
-int WordIdxInStrRev(const char *str,const char c);
-int WordIdxInStrRev(const int si,const char *str,const char c);
-int WordIdxInStrRev(const int si,const int ei,const char *str,const char c);
-
-PO2 SymBlkIdxInStr(const int si,const int ei,const char *str,const char *SymChrLst);
 
 class MyStr {
 private:
-			int  		len;
-			char 		*str;
-			char 		*tmp;
+static 		 char 		Emptystr[];
+static 		 PO2		sp2;
+			 int  		len;
+			 char 		*str;
+			 char 		*tmp;
 protected:
-			const char*	resizeX(char **pstr,const char *istr);
-virtual 	const char*	resize(const char *istr);
-virtual 	const char*	resizetmp(const char *istr);
-virtual 	void		release(void);
-virtual 	void		releasetmp(void);
+			 const char*	resizeX(char **pstr,const char *istr);
+virtual 	 const char*	resize(const char *istr);
+virtual 	 const char*	resizetmp(const char *istr);
+virtual 	 void		release(void);
+virtual 	 void		releasetmp(void);
 
 public:
-			MyStr();
-			MyStr(const char *s);
-			MyStr(MyStr &S);
-			~MyStr();
+			 MyStr();
+			 MyStr(const char *s);
+			 MyStr(MyStr &S);
+			 ~MyStr();
 			
-virtual 	operator int();
-virtual 	operator char*();
-virtual 	operator const char*();
+virtual 	 operator int();
+virtual 	 operator char*();
+virtual 	 operator const char*();
 
-virtual 	MyStr&	operator=(const char *s);
-virtual 	MyStr&	operator=(const char c);
-virtual 	MyStr&	operator=(MyStr &S);
+virtual 	 MyStr& operator=(const char *s);
+virtual 	 MyStr& operator=(const char c);
+virtual 	 MyStr& operator=(MyStr &S);
 
-virtual 	MyStr&	operator+=(const char *s);
-virtual 	MyStr&	operator+=(const char c);
-virtual 	MyStr&	operator+=(MyStr &S);
+virtual 	 MyStr& operator+=(const char *s);
+virtual 	 MyStr& operator+=(const char c);
+virtual 	 MyStr& operator+=(MyStr &S);
 
-virtual 	bool	operator==(const char *s);
-virtual 	bool	operator==(const char c);
-virtual 	bool	operator==(MyStr &S);
+virtual 	 bool operator==(const char *s);
+virtual 	 bool operator==(const char c);
+virtual 	 bool operator==(MyStr &S);
  
-virtual 	bool	operator!=(const char *s);
-virtual 	bool	operator!=(const char c);
-virtual 	bool	operator!=(MyStr &S);
+virtual 	 bool operator!=(const char *s);
+virtual 	 bool operator!=(const char c);
+virtual 	 bool operator!=(MyStr &S);
 
-virtual 	const char*	operator+(const char *s);
-virtual 	const char*	operator+(const char c);
-virtual 	const char*	operator+(MyStr &S);
+virtual 	 const char* operator+(const char *s);
+virtual 	 const char* operator+(const char c);
+virtual 	 const char* operator+(MyStr &S);
 
-virtual 	int		InStr(const char *w);
-virtual 	int		InStr(const int si,const char *w);
-virtual 	int		InStr(const int si,const int ei,const char *w);
+			 PO2 		SymBlkIdxInStr(const int si,const int ei,const char *SymChrLst);
+			 PO2 		SymBlkIdxInStr(const int si,const int ei,const char *str,const char *SymChrLst);
 
-virtual 	int		InStr(const char c);
-virtual 	int		InStr(const int si,const char c);
-virtual 	int		InStr(const int si,const int ei,const char c);
+virtual 	 int 		InStr(const char *w);
+virtual 	 int 		InStr(const int si,const char *w);
+virtual 	 int 		InStr(const int si,const int ei,const char *w);
 
-virtual 	int		InStrRev(const char *w);
-virtual 	int		InStrRev(const int si,const char *w);
-virtual 	int		InStrRev(const int si,const int ei,const char *w);
+virtual 	 int 		InStr(const char c);
+virtual 	 int 		InStr(const int si,const char c);
+virtual 	 int 		InStr(const int si,const int ei,const char c);
 
-virtual 	int		InStrRev(const char c);
-virtual 	int		InStrRev(const int si,const char c);                                              
-virtual 	int		InStrRev(const int si,const int ei,const char c);
+virtual 	 int 		InStrRev(const char *w);
+virtual 	 int 		InStrRev(const int si,const char *w);
+virtual 	 int 		InStrRev(const int si,const int ei,const char *w);
 
-virtual 	const char* GetStrRangeByKeyExSymBlk(const char *k1,const char *k2,const char *SymChrLst);
-virtual 	const char* GetStrRangeBtwKeyExSymBlk(const char *k1,const char *k2,const char *SymChrLst);
+virtual 	 int 		InStrRev(const char c);
+virtual 	 int 		InStrRev(const int si,const char c);                                              
+virtual 	 int 		InStrRev(const int si,const int ei,const char c);
+
+virtual 	 const char* GetRangeByKeyExSymBlk(const char *k1,const char *k2,const char *SymChrLst);
+virtual 	 const char* GetRangeBtwKeyExSymBlk(const char *k1,const char *k2,const char *SymChrLst);
 					
-virtual 	const char* GetStrRangeByKey(const char *k1,const char *k2);
-virtual 	const char* GetStrRangeByKey(const char c1,const char c2);
-virtual 	const char* GetStrRangeBtwKey(const char *k1,const char *k2);
-virtual 	const char* GetStrRangeBtwKey(const char c1,const char c2);
+virtual 	 const char* GetRangeByKey(const char *k1,const char *k2);
+virtual 	 const char* GetRangeByKey(const char c1,const char c2);
+virtual 	 const char* GetRangeBtwKey(const char *k1,const char *k2);
+virtual 	 const char* GetRangeBtwKey(const char c1,const char c2);
 
-virtual 	const char* GetStrRangeByIdx(const char si,const char ei);
-virtual 	const char* GetStrRangeBtwIdx(const char si,const char ei);
+virtual 	 const char* GetRangeByIdx(const char si,const char ei);
+virtual 	 const char* GetRangeBtwIdx(const char si,const char ei);
 
-virtual		const char* GetStrRemoveWord(const char *wd);
-virtual		const char* GetStrRemoveWord(const int si,const char *wd);
-virtual		const char* GetStrRemoveWord(const int si,const int ei,const char *wd);
+virtual 	 const char* GetRangeWithWdChg(const char *owd,const char *nwd);
+virtual 	 const char* GetRangeWithWdChg(const int si,const char *owd,const char *nwd);
+virtual 	 const char* GetRangeWithWdChg(const int si,const int ei,const char *owd,const char *nwd);
 
-virtual		const char* GetStrRemoveWordExSymBlk(const int si,const int ei,const char *wd,const char *SymChrLst);
+virtual 	 const char* GetRangeWithWdDel(const char *wd);
+virtual 	 const char* GetRangeWithWdDel(const int si,const char *wd);
+virtual 	 const char* GetRangeWithWdDel(const int si,const int ei,const char *wd);
+
+virtual 	 const char* GetRangeWithWdChgExSymBlk(const char *owd,const char *nwd,const char *SymChrLst);
+virtual 	 const char* GetRangeWithWdChgExSymBlk(const int si,const char *owd,const char *nwd,const char *SymChrLst);
+virtual 	 const char* GetRangeWithWdChgExSymBlk(const int si,const int ei,const char *owd,const char *nwd,const char *SymChrLst);
+
+virtual 	 const char* GetRangeWithWdDelExSymBlk(const char *wd,const char *SymChrLst);
+virtual 	 const char* GetRangeWithWdDelExSymBlk(const int si,const char *wd,const char *SymChrLst);
+virtual 	 const char* GetRangeWithWdDelExSymBlk(const int si,const int ei,const char *wd,const char *SymChrLst);
 
 };
 
